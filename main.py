c5621e8527f8c386e3d21e6e8090b54b3fdbc787
Freek van Sambeek <69403848+freek-van-sambeek@users.noreply.github.com>
2022-03-17 16:17:01 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import csv
 # Testing
 import pandas as pd
 import statsmodels.formula.api as sm
-
+import timeit
 
 path = "/Users/martin/Downloads/Econometrics/Housing.csv"
 data = []
@@ -15,10 +15,16 @@ with open(path, 'r') as file:
         data.append([i for i in row[:4]])
         # data.append([i for i in row)
 
+start = timeit.default_timer()
 model = Regression(data, header=True)
-print(model.ols())
+print(model.ols().data)
+stop = timeit.default_timer()
+print("Own ran in: ", stop - start)
 
+start = timeit.default_timer()
 df = pd.read_csv(path)
 df_skim = df[['price', 'area', 'bedrooms', 'bathrooms']]
 result = sm.ols(formula="price ~ area + bedrooms + bathrooms - 1", data=df).fit()
 print(result.params)
+stop = timeit.default_timer()
+print("SM ran in: ", stop - start)
